ad38c6bf77574e21fd9bea2d1bc5958510269c6a
ThermiteX <39736661+thermitex@users.noreply.github.com>
2019-07-09 17:46:46 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,43 @@
+var username = "root";
+var greeting;
+var today = new Date();
+var hour = today.getHours();
+var morninghints = ["A good day starts with a good lab.", 
+                    "Early rise for the knowledge?", 
+                    "Nice seeing a refreshed you there."];
+var afternoonhints = ["No food or drink is allowed in the lab.", 
+                    "How about typing \"afternoon tea\" in the command?", 
+                    "Jerry here. I am getting sleepy. I am... zzz"];
+var eveninghints = ["Still working? Don't worry, I am with you.", 
+                    "Today is a full moon. I guess?", 
+                    "A dinner in the lab is better than anywhere."];
+var hint;
+var index = Math.floor(Math.random() * 3);
+
+if (hour > 5 && hour < 12) {
+    greeting = "Good morning, ";
+    hint = morninghints[index];
+} else if (hour < 19) {
+    greeting = "Good afternoon, ";
+    hint = afternoonhints[index];
+} else {
+    greeting = "Good evening, ";
+    hint = eveninghints[index];
+}
+
 $(document).ready(function(){
+
+    $("#head").html(greeting + username + "!<br>" + hint);
+
     $("button").click(function(){
       $("#output").html("");
     });
 
     var count = 0;
+
+    function showError(type, description) {
+        return "<span class=\"errtext\">" + type +": </span>" + description;
+    }
 
     function eval(input){
         commands = input.split(" ");
@@ -11,21 +45,19 @@ $(document).ready(function(){
             case "return":
                 switch (commands[1]) {
                     case "date":
-                        var today = new Date();
+                        today = new Date();
                         var dd = today.getDate();
                         var mm = today.getMonth() + 1;
                         var yyyy = today.getFullYear();
-                        var date = yyyy + '-' + mm + '-' + dd;
-                        return date;
+                        return yyyy + '-' + mm + '-' + dd;
                     case "time":
-                        var today = new Date();
-                        var now = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-                        return now;
+                        today = new Date();
+                        return today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
                     default:
                         return commands;
                 }
             default:
-                return "\'" + input + "\'" + " is an invalid command";
+                return showError("SYNTAX_ERROR", "\'" + commands[0] + "\'" + " is an invalid command");
         }
     }
 
@@ -35,7 +67,7 @@ $(document).ready(function(){
         if (input != "") {
             count++;
             $("#output").html(function(i, org) {
-                return org + "Command[" + count + "]: " + eval(input) + "<br>";
+                return "<span class=\"cmdtext\">Command[" + count + "]: </span>" + eval(input) + "<br>" + org;
             })
             $("#command").val("");
         }
